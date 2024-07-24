4e7666fd6687d9ede4c3c8c19beb9f63b2fe3778
junwai7159 <junwai7159@gmail.com>
2024-07-24 14:36:28 +0800

    Updated Heroes to Achievements

--- app/components/pages/Heroes.tsx
+++ app/components/pages/Heroes.tsx
@@ -14,7 +14,7 @@ export default async function Heroes() {
   return (
     <section className="mt-32 max-w-5xl">
       <Slide delay={0.17}>
-        <h2 className="text-4xl mb-4 font-bold tracking-tight">Heroes</h2>
+        {/* <h2 className="text-4xl mb-4 font-bold tracking-tight">Heroes</h2>
         <p className="dark:text-zinc-400 text-zinc-600 max-w-2xl">
           Inspired by{" "}
           <RefLink
@@ -28,10 +28,17 @@ export default async function Heroes() {
           <strong className="font-semibold">
             &quot;In no particular order&quot;
           </strong>
-        </p>
+        </p> */}
+
+        <h2 className="text-4xl mb-4 font-bold tracking-tight">Achievements</h2>
+        <p className="dark:text-zinc-400 text-zinc-600 max-w-2xl">Here are some achievements that I'm proud of:</p>
+        <h3 className="font-incognito font-semibold text-2xl mt-6 mb-2">Shanghai Jiao Tong University Outstanding Undergraduate Graduates</h3>
+        <p className="mt-2 ml-2">Awared to one of 22 top graduating International students for the class of 2024.</p>
+        <h3 className="font-incognito font-semibold text-2xl mt-6 mb-2">Shanghai Jiao Tong University International Student Scholarship</h3>
+        <p className="mt-2 ml-2">Awared a half tuition fee waiver for four consecutive years.</p>
       </Slide>
 
-      <ul className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-6 mt-12 tracking-tight">
+      {/* <ul className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-6 mt-12 tracking-tight">
         {heroes.map((heroe) => (
           <li
             key={heroe._id}
@@ -48,7 +55,7 @@ export default async function Heroes() {
             </RefLink>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </section>
   );
 }
--- app/components/pages/Usage.tsx
+++ app/components/pages/Usage.tsx
@@ -14,7 +14,7 @@ export default async function Usage() {
     <section className="max-w-2xl">
       <div className="mb-8">
         <h2 className="text-4xl mb-4 font-bold tracking-tight">Usage</h2>
-        <p className="dark:text-zinc-400 text-zinc-600 max-w-xl">
+        <p className="dark:text-zinc-400 text-zinc-600 max-w-xl mb-4">
           Tools, technologies and gadgets I use on a daily basis but not limited
           to.
         </p>
@@ -26,6 +26,47 @@ export default async function Usage() {
           components={CustomPortableTextFavicon}
         />
       ))}
+      <h3 className="font-incognito font-semibold text-2xl mt-6 mb-2">Programming Languages</h3>
+      <ul className="mt-2 ml-2">
+          <li className="mb-4">Python</li>
+          <li className="mb-4">C/C++</li>
+          <li className="mb-4">Java</li>
+          <li className="mb-4">JavaScript</li>
+          <li className="mb-4">TypeScript</li>
+          <li className="mb-4">SQL</li>
+          <li className="mb-4">MATLAB</li>
+        </ul>
+      <h3 className="font-incognito font-semibold text-2xl mt-6 mb-2">Programming Frameworks</h3>
+        <ul className="mt-2 ml-2">
+          <li className="mb-4">PyTorch</li>
+          <li className="mb-4">TensorFlow</li>
+          <li className="mb-4">OpenCV</li>
+          <li className="mb-4">NumPy</li>
+          <li className="mb-4">Pandas</li>
+          <li className="mb-4">SciPy</li>
+          <li className="mb-4">Matplotlib</li>
+          <li className="mb-4">Seaborn</li>
+        </ul>
+      <h3 className="font-incognito font-semibold text-2xl mt-6 mb-2">Technologies</h3>
+        <ul className="mt-2 ml-2">
+          <li className="mb-4">Git</li>
+          <li className="mb-4">Node</li>
+          <li className="mb-4">React</li>
+          <li className="mb-4">Express</li>
+          <li className="mb-4">Nginx</li>
+          <li className="mb-4">MongoDB</li>
+          <li className="mb-4">MySQL</li>
+          <li className="mb-4">Docker</li>
+          <li className="mb-4">Qt</li>
+        </ul>
+      <h3 className="font-incognito font-semibold text-2xl mt-6 mb-2">Tools</h3>
+      <h3 className="font-incognito font-semibold text-2xl mt-6 mb-2">Platform</h3>
+        <ul className="mt-2 ml-2">
+          <li className="mb-4">GitHub</li>
+          <li className="mb-4">Vercel</li>
+          <li className="mb-4">Render</li>
+          <li className="mb-4">Steam</li>
+        </ul>
     </section>
   );
 }
