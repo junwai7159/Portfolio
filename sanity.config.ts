a643dd614bb2cd800e0399c1b508494321841120
junwai7159 <junwai7159@gmail.com>
2024-08-07 19:43:23 +0800

    Updated README.md

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,7 @@ import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 import { codeInput } from "@sanity/code-input";
 import { table } from "@sanity/table";
+
 import { projectId, dataset } from "./lib/env.api";
 
 export default defineConfig({
