4ce5ce33283bb3ac25a9702c72cebfff10a33733
junwai7159 <junwai7159@gmail.com>
2024-08-17 11:23:02 +0800

    Revert "Updated README.md"
    This reverts commit a643dd614bb2cd800e0399c1b508494321841120.

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,6 @@ import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 import { codeInput } from "@sanity/code-input";
 import { table } from "@sanity/table";
-
 import { projectId, dataset } from "./lib/env.api";
 
 export default defineConfig({
